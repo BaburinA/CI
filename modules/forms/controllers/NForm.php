b52843f146e92fc86774b06166126cf7ece3906b
Alexander <bagik61@mail.ru>
2022-03-07 17:08:59 +0400

    New files controller

---
+++ modules/forms/controllers/NForm.php
@@ -0,0 +1,24 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Forms extends MX_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+        $this->load->library('common/main_lib');
+        $this->load->helper('security');
+        $this->load->model('forms_model');
+        //$this->load->controller('page');
+	}
+
+    function new_formalizer() {
+        $is_ajax = $this->input->is_ajax_request();
+        if ($is_ajax) {
+            $post = $this->input->post();
+            $post = $this->security->xss_clean($post);
+            
+        }
+
+    }
+}
+?>
